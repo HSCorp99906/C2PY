52953e2dfe7e472985a046dcff00c84e570b8c9e
Ian Moffett <teaqllabs@gmail.com>
2021-12-07 19:05:45 -0500

    Added tests and more tokens.

--- src/Lexer.py
+++ src/Lexer.py
@@ -31,7 +31,7 @@ class Token:
 
 	def __repr__(self):
 		if self.value:
-			return f'{self.type}:{self.value}'
+			return f'{self.type}: {self.value}'
 
 		return f'{self.type}'
 
@@ -44,47 +44,62 @@ class Lexer:
 
 	def advance(self):
 		self.pos += 1
-		self.current_char = self.text[pos] if self.pos < len(self.text) else None
+		self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
 
-	def peek(self, offset: int) -> str:
-		peak_char = self.text[pos + offset] if self.pos < len(self.text) else None
+	def peek(self, offset: int, sub=False) -> str:
+		if not sub:
+			peak_char = self.text[self.pos + offset] if self.pos < len(self.text) else None
+		else:
+			peak_char = self.text[self.pos - offset] if self.pos < len(self.text) else None
+
 		return peak_char
 
 	def make_tokens(self) -> (list, Error):
 		tokens = []
 
+		self.advance()
+
 		while self.current_char != None:
 			if self.current_char in '\t':
 				self.advance()
 			elif self.current_char in DIGITS:
-				
+				tok = ""
+
 				while self.current_char in DIGITS:
+					tok += self.current_char
 					self.advance()
 				else:
 					if self.current_char == "." and self.peek(1) in DIGITS:
-						tokens.append(Token(TT_FLOAT))
 						self.advance()
+						tok += "."
+						while self.current_char in DIGITS:
+							tok += self.current_char
+							self.advance()
+
+						tokens.append(Token(TT_FLOAT, tok))
 					elif self.current_char == "." and self.peek(1) not in DIGITS:
-						return UnexpectedTokenError("Expected integer literal after \".\".")
+						return UnexpectedTokenError("Expected integer literal after \".\"")
 					else:
-						tokens.append(Token(TT_INT))
+						tokens.append(Token(TT_INT, tok))
 			elif self.current_char == '+':
-				tokens.append(Token(TT_PLUS))
+				tokens.append(Token(TT_PLUS, self.current_char))
 				self.advance()
 			elif self.current_char == '-':
-				tokens.append(Token(TT_MINUS))
+				tokens.append(Token(TT_MINUS, self.current_char))
 				self.advance()
 			elif self.current_char == '*':
-				tokens.append(Token(TT_MUL))
+				tokens.append(Token(TT_MUL, self.current_char))
 				self.advance()
 			elif self.current_char == '/':
-				tokens.append(Token(TT_DIV))
+				tokens.append(Token(TT_DIV, self.current_char))
 				self.advance()
 			elif self.current_char == '(':
-				tokens.append(Token(TT_LPAREN))
+				tokens.append(Token(TT_LPAREN, self.current_char))
 				self.advance()
 			elif self.current_char == ')':
-				tokens.append(Token(TT_RPAREN))
+				tokens.append(Token(TT_RPAREN, self.current_char))
+				self.advance()
+			else:
 				self.advance()
 
 		return tokens
--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
 import sys
 import os
+from Lexer import *
 
 
 def getcontents(filename: str) -> str:
 	with open(filename, "r") as src:
 		return src.read()
 
+def tokenize(contents: str):
+	lexer = Lexer(contents)
+	tokens = lexer.make_tokens()
+
+	if isinstance(tokens, Error):
+		print(tokens.as_string())
+		exit(1)
+	else:
+		return tokens
+
+
+if len(sys.argv) < 2:
+	print(f"Usage: {sys.argv[0]} <file> ..")
+	exit(1)
 
 for i in sys.argv:
 	if not os.path.exists(i):
@@ -13,5 +28,5 @@ for i in sys.argv:
 		exit(1)
 
 	if i != "main.py":
-
-
+		contents = getcontents(i)
+		print(tokenize(contents))
